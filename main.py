329a71e0c6aa37a17f6a1b5bc49959ef0d726ca7
h21anlus <h21anlus@du.se>
2022-03-10 15:26:54 +0100

    LEGENDARY

--- main.py
+++ main.py
@@ -49,11 +49,32 @@ data = pd.read_csv('Pokemon.csv')
 
 # konverterar csv till excel / skapar tabell
 # kräver extension Excel Viewer
-wb = Workbook()
-ws = wb.active
-with open('Pokemon.csv', 'r') as f:
-    for row in csv.reader(f):
-        ws.append(row)
-wb.save('Pokemon.xlsx')
+# wb = Workbook()
+# ws = wb.active
+# with open('Pokemon.csv', 'r') as f:
+#     for row in csv.reader(f):
+#         ws.append(row)
+# wb.save('Pokemon.xlsx')
 
-# söka på en specifik pokemon och få fram all info om den
+# meny för att söka på en specifik pokemon och få fram all info om den
+while [1, 2]:
+    print("Sök på en Pokémon, 1. För att söka med nummer. 2. För att söka med namn.")
+    menu = int(input("Välj 1 eller 2: "))
+
+    if menu == 1:
+        print("Sök på Pokémon med nummer!")
+        userPoke = input()
+        data_indexed = pd.read_csv('Pokemon.csv', index_col="Name")
+        test = data_indexed.iloc[[userPoke]][['Type 1','Type 2','Total','HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Generation', 'Legendary']] # söker på index
+        print(test)
+
+    elif menu == 2:
+        print("Sök på Pokémon med namn!") # Måste börja med stor bokstav
+        userPoke = input()
+        data_indexed = pd.read_csv('Pokemon.csv', index_col="Name")
+        test = data_indexed.loc[[userPoke]][['Type 1','Type 2','Total','HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Generation', 'Legendary']] # söker på namn
+        print(test)
+    
+    else:
+        print("Programmet avslutas")
+        break
