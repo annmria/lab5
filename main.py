86188e21f66afe55eb9284ab6ef9ae918f192114
Chelsan98 <96055501+Chelsan98@users.noreply.github.com>
2022-03-16 18:18:12 +0100

    fixade felhantering

--- main.py
+++ main.py
@@ -6,9 +6,12 @@ import matplotlib.pyplot as plt
 import csv
 import seaborn as sns
 from openpyxl import Workbook
-
-data = pd.read_csv('Pokemon.csv')
-# print(data)
+try:
+    data = pd.read_csv('Pokemon.csv')
+except:
+    FileNotFoundError 
+    print("filen finns inte eller så är namnet fel")
+    
 
 # name = data['Name']
 # type1 = data['Type 1']
@@ -45,8 +48,8 @@ data = pd.read_csv('Pokemon.csv')
 # sorterar efter ex hp, namn... (lägst till högst, a - z)
 # visar endast angivna
 # ta bort values så visas allt
-dataTable = pd.pivot_table(data, index=["Name"], values=["HP", "Total"])
-print(dataTable)
+#dataTable = pd.pivot_table(data, index=["Name"], values=["HP", "Total"])
+#print(dataTable)
 
 # konverterar csv till excel / skapar tabell
 # kräver extension Excel Viewer
@@ -58,26 +61,34 @@ print(dataTable)
 # wb.save('Pokemon.xlsx')
 
 # meny för att söka på en specifik pokemon och få fram all info om den
-# while [1, 2]:
-#     print("Sök på en Pokémon, 1. För att söka med nummer. 2. För att söka med namn.")
-#     menu = int(input("Välj 1 eller 2: "))
+while [1, 2]:
+    print("Sök på en Pokémon, 1. För att söka med nummer. 2. För att söka med namn.")
+    menu = int(input("Välj 1 eller 2: "))
 
-#     if menu == 1:
-#         print("Sök på Pokémon med nummer!")
-#         userPoke = input()
-#         data_indexed = pd.read_csv('Pokemon.csv', index_col="Name")
-#         test = data_indexed.iloc[[userPoke]][['Type 1','Type 2','Total','HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Generation', 'Legendary']] # söker på index
-#         print(test)
+    if menu == 1:
+        print("Sök på Pokémon med nummer!")
+        userPoke = input()
+        data_indexed = pd.read_csv('Pokemon.csv', index_col="Name")
+        try:
+            test = data_indexed.iloc[[userPoke]][['Type 1','Type 2','Total','HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Generation', 'Legendary']] # söker på index
+            print(test)
+        except:
+            KeyError
+            print("Du skev fel prova igen.")
 
-#     elif menu == 2:
-#         print("Sök på Pokémon med namn!") # Måste börja med stor bokstav
-#         userPoke = input()
-#         data_indexed = pd.read_csv('Pokemon.csv', index_col="Name")
-#         test = data_indexed.loc[[userPoke]][['Type 1','Type 2','Total','HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Generation', 'Legendary']] # söker på namn
-#         print(test)
+    elif menu == 2:
+        print("Sök på Pokémon med namn!") # Måste börja med stor bokstav
+        userPoke = input()
+        data_indexed = pd.read_csv('Pokemon.csv', index_col="Name")
+        try:
+            test = data_indexed.loc[[userPoke]][['Type 1','Type 2','Total','HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Generation', 'Legendary']] # söker på namn
+            print(test)
+        except:
+            KeyError
+            print("Du skev fel prova igen. Kolla stavning och ha stor bokstav först.")
     
-#     else:
-#         print("Programmet avslutas")
-#         break
+    else:
+        print("Programmet avslutas")
+        break
 
 #print(data)
