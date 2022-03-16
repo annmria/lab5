e90bcc24a9a0741dce9e26996aa1270e0b62b616
Chelsan98 <96055501+Chelsan98@users.noreply.github.com>
2022-03-16 15:55:18 +0100

    mindre ändringar

--- main.py
+++ main.py
@@ -19,13 +19,14 @@ data = pd.read_csv('Pokemon.csv')
 
 # medelvärde
 # Total,HP,Attack,Defense,Sp. Atk,Sp. Def,Speed
-# attack_mean = data['Attack'].head() -.mean()
+# attack_mean = data['Attack'].tail().mean()
+# print (attack_mean)
 
-# visar en graf med alla stats
-# data.plot(title='Pokemon attack', kind='line')
-# plt.ylabel('#') # label = namnet på axeln
-# plt.xlabel('Attack')
-# plt.show()
+#visar en graf med alla stats
+#data.plot(title='Pokemon stats', kind='line')
+#plt.ylabel('stats') # label = namnet på axeln
+#plt.xlabel('Pokemon')
+#plt.show()
 
 # visar graf på generationernas attack stat
 # fig, scat = plt.subplots(figsize=(10, 6))
@@ -57,24 +58,26 @@ data = pd.read_csv('Pokemon.csv')
 # wb.save('Pokemon.xlsx')
 
 # meny för att söka på en specifik pokemon och få fram all info om den
-while [1, 2]:
-    print("Sök på en Pokémon, 1. För att söka med nummer. 2. För att söka med namn.")
-    menu = int(input("Välj 1 eller 2: "))
+# while [1, 2]:
+#     print("Sök på en Pokémon, 1. För att söka med nummer. 2. För att söka med namn.")
+#     menu = int(input("Välj 1 eller 2: "))
 
-    if menu == 1:
-        print("Sök på Pokémon med nummer!")
-        userPoke = input()
-        data_indexed = pd.read_csv('Pokemon.csv', index_col="Name")
-        test = data_indexed.iloc[[userPoke]][['Type 1','Type 2','Total','HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Generation', 'Legendary']] # söker på index
-        print(test)
+#     if menu == 1:
+#         print("Sök på Pokémon med nummer!")
+#         userPoke = input()
+#         data_indexed = pd.read_csv('Pokemon.csv', index_col="Name")
+#         test = data_indexed.iloc[[userPoke]][['Type 1','Type 2','Total','HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Generation', 'Legendary']] # söker på index
+#         print(test)
 
-    elif menu == 2:
-        print("Sök på Pokémon med namn!") # Måste börja med stor bokstav
-        userPoke = input()
-        data_indexed = pd.read_csv('Pokemon.csv', index_col="Name")
-        test = data_indexed.loc[[userPoke]][['Type 1','Type 2','Total','HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Generation', 'Legendary']] # söker på namn
-        print(test)
+#     elif menu == 2:
+#         print("Sök på Pokémon med namn!") # Måste börja med stor bokstav
+#         userPoke = input()
+#         data_indexed = pd.read_csv('Pokemon.csv', index_col="Name")
+#         test = data_indexed.loc[[userPoke]][['Type 1','Type 2','Total','HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed', 'Generation', 'Legendary']] # söker på namn
+#         print(test)
     
-    else:
-        print("Programmet avslutas")
-        break
+#     else:
+#         print("Programmet avslutas")
+#         break
+
+print(data)
